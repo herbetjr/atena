04f67ff4b42cec8d35e7edf2e638081d2ea033bb
herbetjr <herbetjr@gmail.com>
2019-03-05 14:49:52 -0300

    up

--- resources/views/unica.blade.php
+++ resources/views/unica.blade.php
@@ -5,16 +5,16 @@
     <title>ATENA SOLUCÕES CRIATIVAS</title>
     <meta http-equiv="cache-control"   content="no-cache" />
     <meta http-equiv="pragma" content="no-cache" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/estilo.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilo.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 <body >
-    <header class="container-fluid">
+ <header class="container-fluid">
         <div class="row">
             <div class="col-sm-4 text-center">
                 <img src="imagens/logo-branca.png" style="width: 200px;">
@@ -36,7 +36,7 @@
                         <a class="nav-link scroll" href="#quem-somos" >QUEM SOMOS</a>
                     </li>
                     <li class="nav-item" >
-                        <a class="nav-link" href="#">NOSSO TIME</a>
+                        <a class="nav-link scroll" href="#nosso-time">NOSSO TIME</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link"  href="#">SERVIÇOS</a>
@@ -59,59 +59,59 @@
 
 </header>
 
-    <style type="text/css">
-    .img1{
-        animation-name: a;
-        animation-duration: 25s;
-        animation-iteration-count: infinite;
-        opacity: 0.0;
-    }
-        .img2{
-        animation-name: b;
-        animation-duration: 25s;
-        animation-iteration-count: infinite;
-        opacity: 0.0;
-    }
+<style type="text/css">
+.img1{
+    animation-name: a;
+    animation-duration: 25s;
+    animation-iteration-count: infinite;
+    opacity: 0.0;
+}
+.img2{
+    animation-name: b;
+    animation-duration: 25s;
+    animation-iteration-count: infinite;
+    opacity: 0.0;
+}
 
-    .img3{
-        animation-name: c;
-        animation-duration: 25s;
-        animation-iteration-count: infinite;
-        opacity: 0.0;
-    }
-
-
-    @keyframes a {
-
-        0%   {opacity: 0.0;}]
-         5%   {opacity: 0.25;}
-        25%  {opacity: 1.0;}
-        50%{ opacity:0.0;}
-        75%  {opacity: 0.0;}
-        100%{opacity: 0.0; }
-    }
-      @keyframes b {
-
-        0%   {opacity: 0.0;}
-         25%  {opacity: 0.0;}
-        50%{ opacity: 1.0;}
-          75%{ opacity: 0.0;}
-        100%{opacity: 0.0; }
-    }
-
-      @keyframes c{
-
-         0%   {opacity: 0.0;}
-         25%  {opacity: 0.0;}
-        50%{ opacity: 0.0;}
-          75%{ opacity: 1.0;}
-        100%{opacity: 0.0; }
-    }
-
-    
+.img3{
+    animation-name: c;
+    animation-duration: 25s;
+    animation-iteration-count: infinite;
+    opacity: 0.0;
+}
 
 
-   
+@keyframes a {
+
+    0%   {opacity: 0.0;}]
+    5%   {opacity: 0.25;}
+    25%  {opacity: 1.0;}
+    50%{ opacity:0.0;}
+    75%  {opacity: 0.0;}
+    100%{opacity: 0.0; }
+}
+@keyframes b {
+
+    0%   {opacity: 0.0;}
+    25%  {opacity: 0.0;}
+    50%{ opacity: 1.0;}
+    75%{ opacity: 0.0;}
+    100%{opacity: 0.0; }
+}
+
+@keyframes c{
+
+   0%   {opacity: 0.0;}
+   25%  {opacity: 0.0;}
+   50%{ opacity: 0.0;}
+   75%{ opacity: 1.0;}
+   100%{opacity: 0.0; }
+}
+
+
+
+
+
 
 
 
@@ -119,51 +119,124 @@
 
 </style>
 <section class="banner container-fluid">
-    <div style="position: relative; text-align: center; padding-top: 230px; color: white; width: 500px; max-width: 75%; margin: 0 auto; z-index: 99999999">
+    <div style="position: relative; text-align: center; padding-top: 230px; color: white; width: 500px; max-width: 75%; margin: 0 auto; z-index: 999">
         <h1 style="box-shadow: 1px 1px 30px gray;text-shadow: 4px 4px 10px black; font-size: 50px; border: 4px solid  white; padding: 20px 0px 20px 0px; background-color: rgba(0,0,0,0.5);">Solucões criativas</h1>
     </div>
     <div style="position: absolute; top: 180px;  width: 98%;">
         <center>
-        <img class="img1" src="imagens/desenvolvimento.png" width="250px" style="position: absolute;">
-         <img class="img2" src="imagens/design.png" width="250px" style="position: absolute;">
-        <img class="img3" src="imagens/marketing.png" width="250px">
-    </center>
+            <img class="img1" src="imagens/desenvolvimento.png" width="250px" style="position: absolute;">
+            <img class="img2" src="imagens/design.png" width="250px" style="position: absolute;">
+            <img class="img3" src="imagens/marketing.png" width="250px">
+        </center>
     </div>
 
 </section>
 <section class="quem-somos container-fluid" id="quem-somos" > 
     <div class="row">
-
-        <div class="col-sm-12 " style="text-align: justify;">
+        <div class="col-sm-12 mb-3 ">
             <p class="text-center mt-4"><img src="imagens/templo.png" width="90px;"></p>
             <h1 style="width: 300px; text-align: center; margin: 10px  auto; border-radius: 5px; border-top: 2px solid black;">QUEM SOMOS</h1>
+        </div>
+
+        <div class="col-sm-6 " style="text-align: justify;">
+
 
             Com nome inspirado na  Atena, deusa da sabedoria e artes, pertencente a mitologia grega. A Startup Atena- Soluções Criativas busca enfrentar cada desafio proposto por nossos clientes de maneira criativa e rápida para entregar os melhores resultados possíveis.<br> <br>
 
             Trabalhamos com Desenvolvimento web, Publicidade, Web design e Marketing Digital, conciliando comprometimento e seriedade com capacitação e evolução tecnológica, buscando como princípio fundamental a satisfação do cliente.
             <br><br>
         </div>
-        <div class="col-sm-12">
-            <div class="row">
+        <div class="col-sm-6">
+            <div class="row text-center">
                 <div class="col-sm-4">
-                 <b>Nossa Missão</b> <br>
+                    <i class="material-icons" style="font-size: 50px;">
+                        my_location
+                    </i><br>
+                    <b>Nossa Missão</b> <br>
 
-                 Permitir às pessoas e empresas incorporar a tecnologia ao seu negócio. <br><br>
-             </div>
-             <div class="col-sm-4">
-                 <b>Nossa Visão</b> <br>
+                    Permitir às pessoas e empresas incorporar a tecnologia ao seu negócio. <br><br>
+                </div>
+                <div class="col-sm-4">
+                    <i class="material-icons" style="font-size: 50px;">
+                        assessment
+                    </i><br>
+                    <b>Nossa Visão</b> <br>
 
-                 Disponibilizar às pessoas e empresas sites e sistemas de alta qualidade com ótimo custo benefício. <br><br>
-             </div>
-             <div class="col-sm-4">
-
-                <b>Nossos Valores</b> <br>
-                Agilidade- Transparência- Design- Inovação- Confiança
+                    Disponibilizar às pessoas e empresas sites e sistemas de alta qualidade com ótimo custo benefício. <br><br>
+                </div>
+                <div class="col-sm-4">
+                    <i class="material-icons" style="font-size: 50px;">
+                        assistant
+                    </i><br>
+                    <b>Nossos Valores</b> <br>
+                    Agilidade- Transparência- Design- Inovação- Confiança
+                </div>
             </div>
         </div>
     </div>
-</div>
 </section>
+<section class="nosso-time container-fluid" id="nosso-time" > 
+    <div class="row pb-3" style="background-color: rgba(0,0,0,0.7);">
+        <div class="col-sm-12">
+            <h1 style="width: 300px; text-align: center; margin: 10px  auto; border-radius: 5px; border-bottom: 2px solid white;">#TEAM ATENA</h1>
+        </div>
+       <div class="perfil col-sm-4" >
+            
+            <a href="https://www.facebook.com/herbet.junior" target="_blank"><div class="foto" style="background-image: url('imagens/imagens-equipe/herbet.jpg'); ">
+                
+            </div></a>
+
+            <div class="legenda-pefil">
+                <h4><b>Herbet Junior</b> </h4>
+
+           CEO | Gestão de desenvolvimento
+                
+            </div>
+
+        </div>   
+          <div class="perfil col-sm-4" >
+            
+            <a href="https://www.facebook.com/evanise.paula" target="_blank"><div class="foto" style="background-image: url('imagens/imagens-equipe/eva.jpeg'); ">
+                
+            </div></a>
+
+            <div class="legenda-pefil">
+                <h4><b>Evanise Paula</b> </h4>
+
+            CEO | Gestão de projetos
+                
+            </div>
+
+        </div>  
+        <div class="perfil col-sm-4" >
+            
+            <a href="https://www.facebook.com/italamay.andrade" target="_blank"><div class="foto" style="background-image: url('imagens/imagens-equipe/itala.jpg'); ">
+                
+            </div></a>
+
+            <div class="legenda-pefil">
+                <h4><b>Itala Maiane</b> </h4>
+
+                Comunicação e Marketing digital
+                
+            </div>
+
+        </div>   
+
+        
+        
+    </div>
+</section>
+<section class="quem-somos container-fluid" id="quem-somos" > 
+    <div class="row">
+        <div class="col-sm-12 mb-3 ">
+            <p class="text-center mt-4"><i class="material-icons" style="font-size: 90px;"> widgets</i></p>
+            <h1 style="width: 300px; text-align: center; margin: 10px  auto; border-radius: 5px; border-top: 2px solid black;"> SERVIÇOS </h1>
+        </div>
+        
+    </div>
+</section>
+
 
 
 
@@ -177,19 +250,18 @@
     });
 </script>
 
-    <!--Start of Tawk.to Script-->
+<!--Start of Tawk.to Script-->
 <script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5b8ee1ffafc2c34e96e83756/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/5b8ee1ffafc2c34e96e83756/default';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
 </script>
 <!--End of Tawk.to Script-->
-
 </body>
 </html>
